e6d655da38c336775a19de71bc1191e869db23cd
arpita <arpitaagrahari004@gmail.com>
2024-10-02 01:37:32 +0530

    Refactor PlatformOverview component styles for better readability and user experience

--- src/components/PlatformOverview.js
+++ src/components/PlatformOverview.js
@@ -28,8 +28,8 @@ const PlatformOverview = ({ className = "" }) => {
         <div className="self-stretch flex flex-row items-end justify-center gap-[88px] max-w-full text-13xl text-white mq450:gap-[22px] mq850:gap-[44px] mq1225:flex-wrap">
           <div className="flex-1 flex flex-col items-center justify-start pt-[135px] pb-32 pr-8 pl-5 box-border gap-[17px] bg-[url('/public/image-23@2x.png')] bg-cover bg-no-repeat bg-[top] min-w-[398px] max-w-full mq450:pt-[88px] mq450:pb-[83px] hover:shadow-xl hover:bg-opacity-70 transition-all duration-300 ease-in-out">
             <img
-              className="w-[612px] h-[408px] relative object-cover hidden max-w-full"
-              alt=""
+              className="w-[612px] h-[408px] rounded-lg relative object-cover hidden max-w-full"
+              alt="VLearnUp Platform Overview"
               src="/image-23@2x.png"
             />
             <h3 className="m-0 relative text-inherit font-semibold font-inherit z-[1] mq450:text-lgi mq850:text-7xl">
@@ -44,31 +44,33 @@ const PlatformOverview = ({ className = "" }) => {
             </div>
           </div>
           <div className="w-[600px] flex flex-col items-start justify-end pt-0 px-0 pb-0.5 box-border min-w-[600px] max-w-full mq850:min-w-full mq1225:flex-1">
-            <div className="self-stretch flex flex-col items-center justify-start pt-[132px] pb-[126px] pr-5 pl-[21px] relative gap-[17px] mq450:pt-[86px] mq450:pb-[82px] mq450:box-border hover:shadow-lg hover:bg-opacity-50 transition-all duration-300 ease-in-out">
-              <div className="w-full h-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px]">
-                <div className="absolute h-full w-full top-[0px] right-[0px] bottom-[3px] left-[0px] rounded-xl bg-silver">
-                  <div className="absolute top-[0px] left-[0px] rounded-xl bg-silver w-full h-full hidden" />
-                </div>
-                <img
-                  className="absolute h-full w-full top-[0px] right-[0px] bottom-[0px] left-[0px] max-w-full overflow-hidden max-h-full [mix-blend-mode:linear-burn] z-[1]"
-                  loading="lazy"
-                  alt=""
-                  src="/vector.svg"
-                />
-              </div>
-              <div className="flex flex-row items-start justify-start py-0 pr-[26px] pl-[25px]">
-                <h3 className="m-0 relative text-inherit font-semibold font-inherit z-[2] mq450:text-lgi mq850:text-7xl">
-                  FOR STUDENTS
-                </h3>
-              </div>
-              <button className="cursor-pointer [border:none] pt-6 px-[39px] pb-[23px] bg-midnightblue rounded-61xl flex flex-row items-start justify-start whitespace-nowrap z-[2] hover:bg-darkslateblue-400 transition-colors duration-200">
-                <div className="h-20 w-[283px] relative rounded-61xl bg-midnightblue hidden" />
-                <b className="relative text-3xl font-poppins text-white text-center z-[1]">
-                  Enter access code
-                </b>
-              </button>
-            </div>
-          </div>
+  <div className="self-stretch flex flex-col items-center justify-start pt-[132px] pb-[126px] pr-5 pl-[21px] relative gap-[17px] mq450:pt-[86px] mq450:pb-[82px] mq450:box-border hover:shadow-2xl hover:bg-opacity-60 transition-all duration-300 ease-in-out bg-white rounded-lg">
+    <div className="w-full h-full absolute top-0 right-0 bottom-0 left-0">
+      <div className="absolute h-full w-full top-0 right-0 bottom-3 left-0 rounded-xl bg-gradient-to-r from-gray-200 to-gray-300 shadow-md" />
+      <img
+        className="absolute h-full w-full top-0 right-0 bottom-0 left-0 object-cover rounded-xl z-1"
+        loading="lazy"
+        alt="Vector illustration"
+        src="/vector.svg"
+      />
+    </div>
+    
+    <div className="flex flex-row items-start justify-start py-0 pr-[26px] pl-[25px]">
+    <h3 className="m-0 relative font-semibold text-4xl text-midnightblue text-center transition-transform duration-300 ease-in-out hover:scale-105 hover:text-blue-600 z-2">
+  FOR STUDENTS
+</h3>
+
+    </div>
+
+    <button className="cursor-pointer flex items-center justify-center pt-4 px-8 pb-3 bg-gradient-to-r from-blue-500 to-blue-700 rounded-full shadow-md transition-transform transform hover:scale-105 hover:bg-blue-600 active:scale-95 duration-300 ease-in-out">
+  <b className="relative text-xl font-poppins text-white text-center z-1">
+    Enter Access Code
+  </b>
+</button>
+
+  </div>
+</div>
+
         </div>
       </div>
     </section>
