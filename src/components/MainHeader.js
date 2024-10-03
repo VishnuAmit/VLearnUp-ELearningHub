81f293a7f85b74f319581c4f7a6e0963715375af
satyam1024 <132131308+satyam1024@users.noreply.github.com>
2024-10-03 19:57:02 +0530

    Update MainHeader.js

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -77,7 +77,8 @@ import SignUpScreen from "../pages/SignUpScreen"; // Adjust the path as necessar
 
 Modal.setAppElement("#root"); // Set your app element for accessibility
 
-const MainHeader = ({ className = "" }) => {
+
+const MainHeader = ({coursesRef,articleRef,featuresRef,exploreRef,aboutRef,className = "" }) => {
   const [showLogin, setShowLogin] = useState(false);
   const [showSignUp, setShowSignUp] = useState(false);
 
@@ -85,6 +86,9 @@ const MainHeader = ({ className = "" }) => {
     setShowLogin(true);
     // setShowSignUp(true);
 
+  };
+  const handleScrollToSection = (ref) => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   const closeModal = () => {
@@ -109,34 +113,40 @@ const MainHeader = ({ className = "" }) => {
             </a>
           </div>
           <div className="w-[1148px] flex flex-row items-end justify-start gap-[78px] max-w-full text-3xl mq850:gap-[19px] mq1225:gap-[39px]">
-            <div className="flex-1 flex flex-col items-start justify-end pt-0 px-0 pb-3 box-border max-w-full">
-              <div className="self-stretch flex flex-row items-start justify-between gap-[20px] mq1500:hidden">
-                <a className="[text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[69px] z-[1]">
-                  Home
-                </a>
-                <div className="flex flex-col items-start justify-start py-0 pr-[23px] pl-0">
-                  <div className="flex flex-row items-start justify-start relative">
-                    <a className="[text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[97px] whitespace-nowrap z-[1]">{`Courses `}</a>
-                    <img
-                      className="h-6 w-6 absolute !m-[0] right-[-21px] bottom-[4.2px] overflow-hidden shrink-0 z-[2]"
-                      alt=""
-                      src="/chevronbottomnormal.svg"
-                    />
-                  </div>
-                </div>
-                <div className="flex flex-col items-start justify-start py-0 pr-1.5 pl-0">
-                  <a className="[text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[83px] z-[1]">
-                    Pricing
-                  </a>
-                </div>
-                <div className="flex flex-col items-start justify-start py-0 pr-[5px] pl-0">
-                  <a className="[text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[87px] z-[1]">
-                    Explore
-                  </a>
-                </div>
-                <a className="[text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[105px] whitespace-nowrap z-[1]">
-                  About Us
-                </a>
+            <div className="flex-1 flex flex-col items-start justify-end pt-0 px-0 box-border max-w-full">
+
+              <div className="self-stretch flex flex-row items-start justify-evenly gap-[20px] mq1400:hidden">
+                
+                <button
+                  className=" h-[60px] w-40 [text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[83px] z-[1] bg-gray-200 shadow-[0px_20px_24px_rgba(0,_0,_0,_0.03)] rounded-mini  text-white text-lg font-bold hover:bg-[#F48C06] "
+                  onClick={() => handleScrollToSection(coursesRef)}
+                >
+                Courses
+                </button>
+
+                <button
+                  className="h-[60px] w-40  [text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[83px] z-[1] bg-gray-200 shadow-[0px_20px_24px_rgba(0,_0,_0,_0.03)] rounded-mini  text-white text-lg font-bold hover:bg-[#F48C06] "
+                  onClick={() => handleScrollToSection(articleRef)}
+                >
+                  Article
+                  
+                </button>
+
+                <button
+                  className="h-[60px] w-40 [text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[83px] z-[1] bg-gray-200 shadow-[0px_20px_24px_rgba(0,_0,_0,_0.03)] rounded-mini  text-white text-lg font-bold hover:bg-[#F48C06] "
+                  onClick={() => handleScrollToSection(exploreRef)}
+                >
+                  Explore
+                  
+                </button>
+
+                <button
+                  className="h-[60px] w-40 [text-decoration:none] relative tracking-[0.02em] font-bold text-[inherit] inline-block min-w-[83px] z-[1] bg-gray-200 shadow-[0px_20px_24px_rgba(0,_0,_0,_0.03)] rounded-mini  text-white text-lg font-bold hover:bg-[#F48C06] "
+                  onClick={() => handleScrollToSection(aboutRef)}
+                >
+                 About Us
+                  
+                </button>
               </div>
             </div>
             <div className="flex flex-row items-start justify-start gap-[26px] max-w-full">
@@ -185,3 +195,4 @@ const MainHeader = ({ className = "" }) => {
 };
 
 export default MainHeader;
+
