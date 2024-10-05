e257c5e540a19099897b719ce1a9fa9a8eedcd9e
BarryByte <abhayraj.12667@gmail.com>
2024-10-05 15:48:29 +0530

    fixed issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,15 @@ const Footer = ({ className = "" }) => {
     >
       <div className="w-full flex flex-col items-center justify-between gap-10 max-w-[1400px] mx-auto">
         <div className="flex flex-col items-center gap-6">
-          <div className="flex items-center gap-6">
+          <div className="relative flex items-center">
             <img
-              className="h-20 w-20"
+              className="h-20 w-20 "
               alt="VLearnUp Logo"
               src="/group-453.svg"
             />
-            <h3 className="text-5xl font-bold tracking-wider">VLearnUp</h3>
+            <h3 className="absolute left-7 text-5xl font-bold tracking-wider text-white z-10">
+              VLearnUp
+            </h3>
           </div>
           <div className="text-2xl font-semibold tracking-wider">
             All-in-One E-Learning Platform
@@ -24,13 +26,23 @@ const Footer = ({ className = "" }) => {
           <b className="text-3xl tracking-wider">
             Subscribe to get our Newsletter
           </b>
-          <div className="flex w-full max-w-lg gap-6">
+          {/* <div className="flex w-full max-w-lg gap-6 ">
             <input
-              className="w-full px-6 py-4 text-black bg-white border border-gray-300 rounded-xl text-xl focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all duration-300 ease-in-out shadow-lg placeholder-gray-500"
+              className="w-full px-4 py-7  bg-[transparent] border border-white  rounded-full text-xl font-poppins focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all duration-300 ease-in-out shadow-lg  font-bold text-white text-center p-0 z-[2]"
               placeholder="Your Email"
               type="text"
             />
-            <button className="px-8 py-4 bg-dimgray text-white rounded-xl text-xl hover:bg-gray-700 transition-all duration-300 ease-in-out shadow-lg">
+            <button className="h-12 px-6  bg-dimgray text-white rounded-full text-3xl hover:bg-gray-700 cursor-pointer transition-all duration-300 ease-in-out shadow-lg">
+              <b>Subscribe</b>
+            </button>
+          </div> */}
+          <div className="flex w-full max-w-lg gap-6 items-center">
+            <input
+              className="w-full px-4 py-6 bg-transparent border border-white rounded-full text-xl font-poppins focus:ring-2 focus:ring-blue-500 focus:outline-none transition-all duration-300 ease-in-out shadow-lg font-bold text-white text-center p-0 z-[2]"
+              placeholder="Your Email"
+              type="text"
+            />
+            <button className="h-12 px-6 bg-dimgray text-white rounded-full text-3xl hover:bg-gray-700 cursor-pointer transition-all duration-300 ease-in-out shadow-lg">
               <b>Subscribe</b>
             </button>
           </div>
