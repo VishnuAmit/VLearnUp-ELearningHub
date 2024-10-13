34e1038ec4e4b061a2f0f66d59a4214178c1eadc
ThunderBlaze <darkthunderstormshiva@gmail.com>
2024-10-13 12:56:18 +0530

    Header made Responsive, Fixed Hero Bg and Fixed Header Position

--- src/pages/ELearning.js
+++ src/pages/ELearning.js
@@ -24,7 +24,7 @@ const ELearning = () => {
   
   const componentAClicked = () => setRefreshDate(new Date())
   return (
-    <div className="w-full items-center relative bg-white overflow-hidden flex flex-col items-end justify-start px-0 pb-0 box-border gap-[160px] leading-[normal] tracking-[normal] mq850:gap-[40px] mq1225:gap-[80px]">
+    <div className="w-full items-center relative bg-white overflow-hidden flex flex-col items-end justify-start px-0 pb-0 box-border gap-[160px] leading-[normal] tracking-[normal] mq850:gap-[40px] mq1225:gap-[80px] overflow-y-scroll " style={{height: "125vh",scrollbarWidth: "none"}}>
       <MainHeader  refreshDate={refreshDate}
       coursesRef={coursesRef}
       articleRef={articleRef}
@@ -76,9 +76,9 @@ const ELearning = () => {
       <LatestUpdates ref={articleRef}/>
       <section className="w-full flex justify-center h-[1130px] absolute !m-[0] top-[0px] right-[0px] left-[0px]">
         <img
-          className="absolute top-[-230px] left-[-98px] w-[2196.5px] h-[1360px]"
+          className="absolute top-[-230px] left-[-98px] w-[300%] h-[1360px] -scale-x-100 mq850:w-[600%]"
           alt=""
-          src="/footer-top-shape.svg"
+          src="/tilt.svg"
         />
         <div className="relative top-[104px] left-[477px] w-[845px] max-w-[845px  ] h-[1026px] bg-[url('/public/pngtransparentgirlusinglaptopthumbnailremovebgpreview-1-1-1@2x.png')] bg-cover bg-no-repeat bg-[top] z-[3]">
           <img
